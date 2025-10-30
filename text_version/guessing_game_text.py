590ecd861cdf80030c296f00f94278ca52ed374e
Megan Doley <y5s53@students.keele.ac.uk>
2025-10-30 13:49:32 +0000

    Adding a guess limit (10) to text version of game, and out of guesses message.

--- text_version/guessing_game_text.py
+++ text_version/guessing_game_text.py
@@ -4,11 +4,13 @@ def play_game():
     number = random.randint(1, 100)
     guess = None
     tries = 0
+    max_tries = 10
 
-    print("Welcome to Number Guessing Game 2!")
+    print("Welcome to Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
+    print(f"you have {max_tries} guesses.")
 
-    while guess != number:
+    while guess != number and tries < max_tries:
         try:
             guess = int(input("Enter your guess: "))
             tries += 1
@@ -20,6 +22,9 @@ def play_game():
                 print(f"Correct! You guessed it in {tries} tries.")
         except ValueError:
             print("Please enter a valid number.")
+
+    if guess != number:
+        print(f"you're out of guesses. The number was {number}.")
 
 if __name__ == "__main__":
     play_game()
